ef9a7078c4a00b45039eb0724df92908cc72f5fa
francesco-strazzullo <me@francescostrazzullo.info>
2016-09-08 15:17:19 +0200

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.resolve(__dirname,'build'),
+    publicPath: '/build/',
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({'process.env': {
+      BUILD_TIME:(new Date()).getTime()
+    }})
+  ],
+  devServer: {
+    host: '0.0.0.0'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        loader: 'url'
+      }
+    ]
+  }
+};
