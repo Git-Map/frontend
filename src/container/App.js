a07afbeb28d88b313c3882401f3a2e3c4a6cc19a
Antonio Dell'Ava <antonio.dellava@gmail.com>
2016-09-21 21:38:31 +0200

    styling splash page

--- src/container/App.js
+++ src/container/App.js
@@ -2,7 +2,23 @@ import React from "react";
 import { connect } from 'react-redux';
 
 const style = {
-  fontFamily:'Open Sans'
+  fontFamily:'Open Sans',
+  fontSize: '90%'
+};
+
+const styleGitMapLogo = {
+  marginTop: '2rem',
+  marginLeft: '2rem'
+};
+
+const stylePayOff = {
+    marginLeft: '2rem'
+};
+
+const styleComeLogo = {
+  position:'absolute',
+  bottom: '2rem',
+  right: '2rem'
 };
 
 export class App extends React.Component{
@@ -10,10 +26,9 @@ export class App extends React.Component{
   render() {
     return (
       <div style={style}>
-        <h1>Git-Map</h1>
-        <img src={require('./logo.svg')}/>
-
-        <img src={require('./come-logo.svg')}/>
+        <img style={styleGitMapLogo} src={require('./logo.svg')}/>
+        <p style={stylePayOff} >Many Languages, many cauntries, many developers</p>
+        <img style={styleComeLogo} src={require('./come-logo.svg')}/>
       </div>
     );
   }
