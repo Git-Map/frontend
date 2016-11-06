38a6c21d7f745f4e44317fa0969f1e6ab9b6d7c6
francesco-strazzullo <me@francescostrazzullo.info>
2016-11-06 14:42:59 +0100

    moved asset

--- src/modules/shared/pages/Home.jsx
+++ src/modules/shared/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const styleGitMapLogo = {
   marginTop: '2rem',
   marginLeft: '3rem'
@@ -33,11 +34,11 @@ export default class Home extends React.Component{
   render() {
     return (
       <div>
-        <img style={styleGitMapLogo} src={require('././gitmap-logo.svg')}/>
+        <img style={styleGitMapLogo} src={require('../assets/gitmap-logo.svg')}/>
         <p style={stylePayOff} >Many Languages, many countries, many developers</p>
         <a style={styleContactUs} href="mailto:info@codingjam.it" target="_black">contact us</a>
         <a href="http://www.comeonline.it/" target="_blank">
-          <img style={styleComeLogo} src={require('././come-logo.svg')}/>
+          <img style={styleComeLogo} src={require('../assets/come-logo.svg')}/>
         </a>
       </div>
     );
