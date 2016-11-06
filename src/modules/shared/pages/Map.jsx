a186df74694fa17287aceaa9dad339468f30068c
francesco-strazzullo <me@francescostrazzullo.info>
2016-11-06 11:52:06 +0100

    added leaflet

--- src/modules/shared/pages/Map.jsx
+++ src/modules/shared/pages/Map.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from 'react-redux';
 import Actions from "../state/actions";
 import _ from 'lodash';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const mapStateToProps = (state) => {
   return {
@@ -9,7 +10,7 @@ const mapStateToProps = (state) => {
   };
 };
 
-class Map extends React.Component{
+class MapPage extends React.Component{
 
   componentDidMount(){
     this.props.dispatch(Actions.fetchCountries());
@@ -17,19 +18,21 @@ class Map extends React.Component{
 
   render() {
 
-    const countries = this.props.countries.map((country,index) => {
-      return (
-        <p key={index}>{country.name} - {country.users}</p>
-      );
-    });
-
+    const position = [51.505, -0.09];
     return (
-      <div>
-          Mappa
-          {countries}
-      </div>
+      <Map center={position} zoom={13}>
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+          </Popup>
+        </Marker>
+      </Map>
     );
   }
 }
 
-export default connect(mapStateToProps)(Map);
+export default connect(mapStateToProps)(MapPage);
