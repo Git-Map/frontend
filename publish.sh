158ac16e4c84ad2c5338ea3cb138e9e1d5e53a2e
francesco-strazzullo <me@francescostrazzullo.info>
2016-12-02 00:53:23 +0100

    Revert "using main page for build"
    This reverts commit 27df9f69c4226f44dc31253b25e4928fa9584679.

--- publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd build
-git init
-git remote add origin git@github.com:Git-Map/Git-Map.github.io.git
-git add .
-git commit -m "Updates"
-git push --force --set-upstream origin master
-echo Published
